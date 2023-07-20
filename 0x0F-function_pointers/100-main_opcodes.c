1e7b1cd93607e957dc72ea56b0a6b415120c9d69
MichelleMavimira <mmavmira961@gmail.com>
2023-07-19 23:05:03 -0700

    correction

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -18,12 +18,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
-	bytes = atoi(argv[1])
-		if (bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
@@ -32,8 +32,7 @@ int main(int argc, char *argv[])
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx ", arr[i])
+		printf("%02hhx ", arr[i]);
 	}
-
 	return (0);
 }
